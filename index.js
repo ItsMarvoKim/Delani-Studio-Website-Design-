30c2d6f024e3df2767ca4cc835f6e06f27878cfe
ItsMarvoKim <marvin.kithinji@student.moringaschool.com>
2022-03-16 15:36:19 +0300

    form functionality

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
 $(document).ready(function(){
-  $("#development-image").click(function(){
-    $("#development-image").slideDown('1500').hide('1000');
-    $("#development").show('1500');
+  $(".dvt-image").click(function(){
+    $(".dvt-image").slideDown('1500').hide('1000');
+    $(".dv-title").show('1500');
   });
-  $("#development").click(function(){
-    $("#development").slideUp('1500');
-    $("#development-image").slideDown('1500');
+  $("#.dv-title").click(function(){
+    $(".dv-title").slideUp('1500');
+    $(".dvt-image").slideDown('1500');
+  });
+});
+
+$(document).ready(function(){
+  $(".dsn-image").click(function(){
+    $(".dsn-image").slideDown('1500').hide('1000');
+    $(".dsn-title").show('1500');
+  });
+  $(".dsn-title").click(function(){
+    $(".dsn-title").slideUp('1500');
+    $(".dsn-image").slideDown('1500');
+  });
+});
+
+$(document).ready(function(){
+  $(".prdct-image").click(function(){
+    $(".prdct-image").slideDown('1500').hide('1000');
+    $(".prdct-title").show('1500');
+  });
+  $(".prdct-title").click(function(){
+    $(".prdct-title").slideUp('1500');
+    $(".prdct-image").slideDown('1500');
   });
 });
 
